e22f0aa454dd8bc6fe70d04f55ce417426b34c62
Masayoshi Fujita <37256032+fujimasa1031@users.noreply.github.com>
2018-09-06 21:51:32 +0900

    微修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ def index
 end
 
 def create
-  @message = @group.messages.new(message_params)
+  @message = @group.messages.build(message_params)
   if @message.save
     redirect_to group_messages_path(@group), notice: "メッセージ送信が完了しました"
   else
