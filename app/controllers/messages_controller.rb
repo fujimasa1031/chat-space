20e060e5e68d090a70aa0d0b6f936f5ed64778c3
Masayoshi Fujita <37256032+fujimasa1031@users.noreply.github.com>
2018-09-08 16:25:26 +0900

    messages_conrollerの修正
    ###Routing error発生
     - コントローラーでデータが取得できていないと推測

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,12 +4,14 @@ before_action :set_group
 
 def index
   @message = Message.find_by(params[:id])
+  @group = Group.find_by(params[:group_id])
   @messages = @group.messages.includes(:user)
 end
 
 
 
 def create
+  @group = Group.find_by(params[:group_id])
   @message = @group.messages.new(message_params)
   if @message.save
     redirect_to group_messages_path(@group), notice: "メッセージ送信が完了しました"
