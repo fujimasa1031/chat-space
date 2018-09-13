af199cb54c96db04df1e7ac08d91c7a712ed33f5
Masayoshi Fujita <37256032+fujimasa1031@users.noreply.github.com>
2018-09-13 14:06:20 +0900

    Incremental.js インデント修正

--- app/assets/javascripts/incremental.js
+++ app/assets/javascripts/incremental.js
@@ -1,25 +1,25 @@
 $(function(){
 
-var searchList = $('#user-search-result');
-var memberList = $('.chat-group-users');
-// 検索処理
-function appendUser(user){
-  var html = `<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name="${user.name}">追加</a>
-              </div>
-              `
-  searchList.append(html);
+  var searchList = $('#user-search-result');
+  var memberList = $('.chat-group-users');
+  // 検索処理
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name="${user.name}">追加</a>
+                </div>
+                `
+    searchList.append(html);
   }
 
-function addMember(userId,userName){
-  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                <input name='group[user_ids][]' type='hidden' value=${userId}>
-                <p class='chat-group-user__name'>${userName}</p>
-                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-              </div>`
-  memberList.append(html);
-}
+  function addMember(userId,userName){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value=${userId}>
+                  <p class='chat-group-user__name'>${userName}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    memberList.append(html);
+  }
 
 
 
@@ -35,10 +35,10 @@ function addMember(userId,userName){
     .done(function(users) {
       searchList.empty();
       if(users.length !== 0){
-      users.forEach(function(user){
-        appendUser(user);
-      });
-    }
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
     })
     .fail(function(){
       alert("検索は失敗です。。。");
