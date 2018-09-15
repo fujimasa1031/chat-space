09bb8bb11bb45a8c6f38771bb58e01d34133a97e
Masayoshi Fujita <37256032+fujimasa1031@users.noreply.github.com>
2018-09-15 12:52:46 +0900

    turbolinksの設定
    errorの原因がturbolinksと思い、実装。
    Undefinedの表示などは解消されない。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,53 +1,53 @@
-$(function(){
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    function buildHTML(message){
+      var img = message.image? `<img src=${message.image}>` : ""
+      var html = `<div class="chat-main__message" data-message-id="${message.id}">
+                    <div class="chat-main__message-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="chat-main__message-time">
+                      ${message.created_at}
+                    </div>
+                    <div class="chat-main__message-text">
+                      ${ message.text }
+                    <div>
+                     <div class="chat-main__message-image">
+                      ${ img }
+                    </div>
+                  </div>`
+      return html;
+    }
 
-  function buildHTML(message){
-    var img = message.image? `<img src=${message.image}>` : ""
-    var html = `<div class="chat-main__message" data-message-id="${message.id}">
-                  <div class="chat-main__message-name">
-                    ${message.user_name}
-                  </div>
-                  <div class="chat-main__message-time">
-                    ${message.created_at}
-                  </div>
-                  <div class="chat-main__message-text">
-                    ${ message.text }
-                  <div>
-                   <div class="chat-main__message-image">
-                    ${ img }
-                  </div>
-                </div>`
-    return html;
-  }
+    function scrollBottom(){
+      $('.chat-main__messages').animate({scrollTop: $('.chat-main__messages')[0].scrollHeight},"fast");
+    }
 
-  function scrollBottom(){
-    $('.chat-main__messages').animate({scrollTop: $('.chat-main__messages')[0].scrollHeight},"fast");
-  }
-
-  $('#new_message').on('submit',function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+    $('#new_message').on('submit',function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__messages').append(html)
+      $('.footer__form--body-message').val("")
+      $(".submit").removeAttr("disabled")
+      $('.hidden').val('')
     })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.chat-main__messages').append(html)
-    $('.footer__form--body-message').val("")
-    $(".submit").removeAttr("disabled")
-    $('.hidden').val('')
-  })
-  .fail(function(){
-    alert('送信に失敗しました');
-    });
+    .fail(function(){
+      alert('送信に失敗しました');
+      });
 
-  scrollBottom();
-  });
+    scrollBottom();
+    });
   // 最初に書いたコード
   // var interval = setInterval(function() {
   //   var id = $('.chat-main__message:last').data('message-id');
@@ -96,24 +96,25 @@ $(function(){
   //   });
   // }
 
-var update = setInterval(function() {
-    if($('.chat-main__message')[0]){
-      var lastId = $('.chat-main__message:last').data('message-id');
-      $.ajax({
-      url: location.href,
-      data: {message: {id: lastId}},
-      dataType: 'json'
-    })
-    .done(function(data){
-      $.each(data,function(i,data){
-        var newHTML = buildHTML(data);
-        $('.chat-main__messages').append(newHTML);
-        scrollBottom();
+  var update = setInterval(function() {
+      if($('.chat-main__message')[0]){
+        var lastId = $('.chat-main__message:last').data('message-id');
+        $.ajax({
+        url: location.href,
+        data: {message: {id: lastId}},
+        dataType: 'json'
+      })
+      .done(function(data){
+        $.each(data,function(i,data){
+          var newHTML = buildHTML(data);
+          $('.chat-main__messages').append(newHTML);
+          scrollBottom();
+        });
       });
-    });
-  }else{
-      var lastId = 0
-      clearInterval(update);
-    }
-  }, 2500 );
+    }else{
+        var lastId = 0
+        clearInterval(update);
+      }
+    }, 2500 );
+  });
 });
